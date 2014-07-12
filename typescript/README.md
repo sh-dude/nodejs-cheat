c39fd4bac87bb6bbcdea4e936d0063889cda3a02
sh-dude <sh-dude@users.noreply.github.com>
2014-07-12 08:52:01 +0300

    Issue #2. Added more TypeScript.

--- typescript/README.md
+++ typescript/README.md
@@ -1,8 +1,14 @@
+## About
+
 TypeScript is a superset of JavaScript (Ecmascript 5/6) from Microsoft that compiles into readable Javascript. Any valid JavaScript code is also valid TypeScript code.
 
-TypeScript supports classes, type hinting, optional arguments, modules (which can be compiled to CommonJS or AMD modules).
+TypeScript supports classes, type hinting, optional arguments, modules (which can be compiled to CommonJS or AMD modules) and other features.
 
-Typescript can interact with JavaScript libraries like jQuery with using of declaration files (which has d.ts suffix).
+Typescript can interact with JavaScript libraries like jQuery with using of declaration files (*.d.ts or *.ts files).
+
+This cheet sheet contains the following two files:
+- index.ts - quick intro to the TypeScript. To run/stop of it use the `make` and `make kill`.
+- browser.ts - simple demo of the TypeScript in the browser. To run of it compile it manuall and open browser.html in the browser.
 
 
 ## Installation
@@ -29,6 +35,12 @@ Generate the CommonJS module `my-module.js`:
 Generate the AMD module `my-module.js`:
 
     tsc -m amd my-module.ts
+
+---
+
+Watch and compile the `my-script.ts` on change:
+
+    tsc -w my-script.ts
 
 
 ## Useful links
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,54 @@
+// Any valid JavaScript is valid TypeScript but not vise versa.
 
-console.log("OK")
-console.log("OKr")
-console.log("O1")
+// To require a Node.js module you need to have a declaration file that should
+// define API of the module. Let's try it for the 'assert' (http://nodejs.org/api/assert.html)
+// module.
+
+// Use a special ///<reference path="..."/> comment to say where the file is located. The path
+// is resolved relative to the directory of this file.
+///<reference path='assert.d.ts' />
+
+// After that the 'require' statement can be used as follows:
+import assert = require('assert')
+
+assert.ok(true)
+
+// Function can have type hinting:
+function returnAsIs(s: string) {
+    return s
+}
+
+assert.strictEqual(returnAsIs("my test"), "my test")
+
+// The following types for typehinting are also useful:
+// - number
+// - string
+// - boolean
+function add(a: number, b: number) {
+    return a + b
+}
+assert.strictEqual(add(2, 3), 5)
+
+// A type for the return value can be specified:
+function returnAsIs_v1(s: string): string {
+    return s
+}
+assert.strictEqual(returnAsIs_v1("foo"), "foo")
+
+// The `any` type can also be used in any case where any value can be used.
+// The signature of the next function is equivalent to this one: function returnAsIs_v2(v)
+function returnAsIs_v2(v: any): any {
+    return v
+}
+assert.strictEqual(returnAsIs_v2(false), false)
+assert.strictEqual(returnAsIs_v2(undefined), undefined)
+assert.strictEqual(returnAsIs_v2(null), null)
+
+/*
+var myFunc1 = function (h: number, w: number) {
+        return h * w;
+
+};
+
+var myFunc2 = (h: number, w: number) => h * w;
+*/
